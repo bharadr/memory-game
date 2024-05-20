38fb9ccbd9e2355358395f9b79735a8feabd262a
Bharad Raghavan <46429739+bharadr@users.noreply.github.com>
2024-05-19 20:41:55 -0400

    I think I got it all done

--- src/components/MemoryBoard.jsx
+++ src/components/MemoryBoard.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import '../App.css'
 import _ from 'lodash';
 
 
-const names = ["Emily", "Michael", "Sarah", "James", "Jessica", "David", "Ashley", "Daniel", "Megan", "Joshua", "Amanda", "Christopher"];
+const pokemon = ["Garchomp", "Hydreigon", "Goodra", "Metagross", "Dragonite", "Salamence", "Tyranitar", "Mew", "Mewtwo", "Flygon", "Volcarona", "Swampert"];
+
+function lowercaseFirstLetter(str) {
+    if (!str) return str; // Handle empty string
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  }
+  
 
 function MemoryCard({name, handleCard}) {
+    const [imageSrc, setImageSrc] = useState('');
+  
+    useEffect(() => {
+        const url = "https://pokeapi.co/api/v2/pokemon/" + lowercaseFirstLetter(name);
+        const fetchImage = async () => {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setImageSrc(data.sprites.front_default);
+        };
+        fetchImage();
+    }, [name]);
+      
     return (
-        <div className="card" onClick={() => handleCard(name)}>{name}</div>
+        <div className="card" onClick={() => handleCard(name)}>
+            <img src={imageSrc} alt="Loaded from API" />
+            <p>{name}</p>
+        </div>
     )
 }
 
@@ -16,7 +40,7 @@ function MemoryBoard({handleCard}) {
     // Initialize an array to hold the MemoryCard components
     const cards = [];
     for (let i = 0; i < 12; i++) {
-        cards.push(<MemoryCard key={names[i]} name={names[i]} handleCard={handleCard}/>);
+        cards.push(<MemoryCard key={pokemon[i]} name={pokemon[i]} handleCard={handleCard}/>);
     }
     // Shuffle Cards
     const shuffledCards = _.shuffle(cards);
